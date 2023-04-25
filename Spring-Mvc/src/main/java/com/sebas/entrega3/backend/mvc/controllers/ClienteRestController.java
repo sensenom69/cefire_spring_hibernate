d07531f6eda43b40582bc0ffe7e54282c7a8e799
Sebas <sensenom69@gmail.com>
2023-04-25 12:18:04 +0200

    suposadament acabat mvc, falta crear dades de prova

---
+++ Spring-Mvc/src/main/java/com/sebas/entrega3/backend/mvc/controllers/ClienteRestController.java
@@ -0,0 +1,26 @@
+package com.sebas.entrega3.backend.mvc.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sebas.entrega3.backend.mvc.models.entity.Cliente;
+import com.sebas.entrega3.backend.mvc.models.services.IClienteService;
+
+@CrossOrigin(origins = {"*"})
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> index(){
+		return clienteService.findAll();
+	}
+}
