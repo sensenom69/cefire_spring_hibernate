a949dab830366a7a9c9a85d7b5bf260c3e6ef991
Sebas <sensenom69@gmail.com>
2023-04-25 13:03:07 +0200

    creades rutes en el controlador

--- Spring-Mvc/src/main/java/com/sebas/entrega3/backend/mvc/controllers/ClienteRestController.java
+++ Spring-Mvc/src/main/java/com/sebas/entrega3/backend/mvc/controllers/ClienteRestController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,5 +43,23 @@ public class ClienteRestController {
 		cliente.setCreateAt(new Date());
 		this.clienteService.save(cliente);
 		return cliente;
+	}
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente currentCliente = this.clienteService.findById(id);
+		currentCliente.setNombre(cliente.getNombre());
+		currentCliente.setApellido(cliente.getApellido());
+		currentCliente.setEmail(cliente.getEmail());
+		this.clienteService.save(currentCliente);
+		return currentCliente;
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		Cliente currentCliente = this.clienteService.findById(id);
+		this.clienteService.delete(currentCliente);
 	}
 }
