3e10a60253aaf09027136192c348e045635bc028
Sebas <sensenom69@gmail.com>
2023-04-26 10:37:44 +0200

    tractament d'errors en show dactors

--- Entrega3/src/main/java/com/sebas/springboot/backend/cine/controllers/ActoresRestController.java
+++ Entrega3/src/main/java/com/sebas/springboot/backend/cine/controllers/ActoresRestController.java
@@ -1,10 +1,13 @@
 package com.sebas.springboot.backend.cine.controllers;
 
-import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,8 +36,17 @@ public class ActoresRestController {
 	}
 	
 	@GetMapping("/actores/{id}")
-	public Actores show(@PathVariable Long id){
-		return actoresService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id){
+		Actores actor = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			actor = actoresService.findById(id);
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("mensaje", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Actores>(actor, HttpStatus.OK);
 	}
 	
 	@PostMapping("/actores")
